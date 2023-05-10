b2ec03f0a889329c886929eea72681dfc1aea925
DariaKorotkevich <120338205+DariaKorotkevich@users.noreply.github.com>
2023-05-11 01:04:15 +0300

    Add files via upload

---
+++ src/main/java/com/example/buysell/repositories/ImageRepository.java
@@ -0,0 +1,7 @@
+package main.java.com.example.buysell.repositories;
+
+import com.example.buysell.models.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
---
+++ src/main/java/com/example/buysell/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package main.java.com.example.buysell.repositories;
+
+import com.example.buysell.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
